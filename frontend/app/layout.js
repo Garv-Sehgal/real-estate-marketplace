287bc0b850c1ec8a2570465106cbbf05307a17f7
Sweeti Kumari <sweeti754940@gmail.com>
2026-02-19 12:26:32 +0530

    updated register and login page

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -6,6 +6,7 @@ export const metadata = {
     manifest: '/manifest.json',
 };
 
+
 export const viewport = {
     themeColor: '#1d4ed8',
     width: 'device-width',
@@ -19,7 +20,9 @@ export default function RootLayout({ children }) {
             <head>
                 <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&family=Raleway:wght@300;400;500;600;700;800&display=swap" rel="stylesheet" />
             </head>
-            <body>{children}</body>
+            <body>
+                {children}
+            </body>
         </html>
     );
 }
